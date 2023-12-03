d18ee1e3fe697cb0472247dabbdce7b288062646
Gilney Junior <gilneyjnr@gmail.com>
2023-12-03 19:44:20 -0300

    feat: implementation of end nodeblock state

--- src/nodename_machine/nodeblock_machine/states/end_nodeblock_state.cpp
+++ src/nodename_machine/nodeblock_machine/states/end_nodeblock_state.cpp
@@ -1,16 +1,15 @@
 #include "end_nodeblock_state.hpp"
 
-
 Nodename::Nodeblock::EndNodeblockState::EndNodeblockState(
-    Nodename::Nodeblock::NodeblockMachine* machine): 
-    Nodename::Nodeblock::NodeblockState(machine) 
+  Nodename::Nodeblock::NodeblockMachine* machine): 
+  Nodename::Nodeblock::NodeblockState(machine) 
 {
-    machine->finish();
+  machine->finish();
 }
 
 Nodename::Nodeblock::EndNodeblockState::~EndNodeblockState() {}
 
 Nodename::Nodeblock::NodeblockState* Nodename::Nodeblock::EndNodeblockState::run() 
 { 
-    return this;
+  return this;
 }
