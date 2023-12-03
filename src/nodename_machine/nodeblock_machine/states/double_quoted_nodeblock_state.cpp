3e70e14b62fbffca82ed5248eb3e04199d12d65f
Gilney Junior <gilneyjnr@gmail.com>
2023-12-03 19:38:43 -0300

    feat: implementation of double quoted nodeblock state

--- src/nodename_machine/nodeblock_machine/states/double_quoted_nodeblock_state.cpp
+++ src/nodename_machine/nodeblock_machine/states/double_quoted_nodeblock_state.cpp
@@ -1,31 +1,31 @@
 #include "double_quoted_escape_nodeblock_state.hpp"
 #include "double_quoted_nodeblock_state.hpp"
+#include "normal_nodeblock_state.hpp"
 
 Nodename::Nodeblock::DoubleQuotedNodeblockState::DoubleQuotedNodeblockState(
-    Nodename::Nodeblock::NodeblockMachine* machine):
-    Nodename::Nodeblock::NodeblockState(machine) {}
+  Nodename::Nodeblock::NodeblockMachine* machine):
+  Nodename::Nodeblock::NodeblockState(machine) {}
 
 Nodename::Nodeblock::DoubleQuotedNodeblockState::~DoubleQuotedNodeblockState() {}
 
 Nodename::Nodeblock::NodeblockState* Nodename::Nodeblock::DoubleQuotedNodeblockState::run()
 {
-    std::istream& in = *(this->machine->getInputStream());
-    std::ostream& out = *(this->machine->getOutputStream());
+  if (this->input.eof())
+    throw "Unexpected end of input."; // TODO: Criar uma excessão para isso
+  
+  char x;
+  this->input >> x;
 
-    if (in.eof())
-        /* TODO: throw new Exception("Fim de stream inesperado.") */;
-    
-    char x;
-    in >> x;
-
-    if (true /* x in <special> - {\, "} */)
-        out << "\\" << x;
-    else if (true /* x in Sigma - ( { " } U <special> ) */)
-        out << x;
-    else if (x == '\\')
-        return new DoubleQuotedEscapeNodeblockState(this->machine);
-    else
-        /* TODO: throw new Exception("caractere inválido.") */;
-    
+  if (x == '"')
+  {
+    this->output << ')';
+    return new NormalNodeblockState(this->machine);
+  }
+  else if (x == '\\')
+    return new DoubleQuotedEscapeNodeblockState(this->machine);
+  else
+  {
+    this->output << x;
     return this;
+  }
 }
